5d3b6803d511766f4afeb16e3f76fd78a18aea33
AkhilRaj1819 <vysyaraju.akhilraju.s89@kalvium.community>
2025-03-19 15:19:53 +0530

    ok

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = 5000;
 
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(8080,async()=>{
+  try{
+    await mongoose.connect('mongodb+srv://akhil031215n:HKU3fQQmC6LvK9Wi@cluster0.wtzd8.mongodb.net/');
+    console.log('server connected sucessfully')
+  }catch(err){
+    console.log(err);
+
+  }
+})
+
