3bc9a507b70e51556eb0bf22ab5209a81f5d4522
AkhilRaj1819 <vysyaraju.akhilraju.s89@kalvium.community>
2025-05-01 11:38:39 +0530

    Add update and delete entity features: new edit page, delete button, and list refresh

--- frontend/src/AllRoutes.jsx
+++ frontend/src/AllRoutes.jsx
@@ -3,6 +3,7 @@ import {Routes,Route} from 'react-router-dom';
 import About from './About'
 import Navbar from './components/navbar';
 import AddEntityPage from './components/AddEntityPage';
+import EditEntityPage from './components/EditEntityPage';
 
 const AllRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AllRoutes = () => {
       <Routes>
         <Route path='/about' element={< About/>}/>
         <Route path='/add-entity' element={<AddEntityPage/>}/>
+        <Route path='/edit-entity/:id' element={<EditEntityPage/>}/>
       </Routes>
     </div>
   )
